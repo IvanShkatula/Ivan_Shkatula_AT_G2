d69b7357dc3ffcf4617bfe5d64f2f2fa5d7601be
Ivan Shkatula <Ivan_Shkatula@tut.by>
2021-04-18 10:57:56 +0300

    add tests

--- src/main/java/tasks/day16/TestDemoqaChooseAnyValue.java
+++ src/main/java/tasks/day16/TestDemoqaChooseAnyValue.java
@@ -1,5 +1,6 @@
 package day16HomeWork;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
@@ -8,14 +9,18 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class TestDemoqaChooseAnyValue extends WebDriverSettings {
 
 
-
     @Test
     public void chooseValuesFromDropdowns() {
         driver.get("https://demoqa.com/select-menu");
-       driver.findElement(By.xpath("//div[@class='accordion']/div[1]")).click();
-       webElement = driver.findElement(By.id("react-select-2-input"));
-       Select select = new Select(webElement);
-       select.selectByValue("Mouse");
-
+        driver.findElement(By.xpath("//select[@id='oldSelectMenu']")).click();
+        webElement = driver.findElement(By.id("oldSelectMenu"));
+        Select select = new Select(webElement);
+        select.selectByValue("1");
+        String colorsText = select.getFirstSelectedOption().getText();
+        webElement = driver.findElement(By.id("cars"));
+        select = new Select(webElement);
+        select.selectByValue("saab");
+        String carsText = select.getFirstSelectedOption().getText();
+        Assert.assertTrue(colorsText.equalsIgnoreCase("blue") && carsText.equalsIgnoreCase("saab"));
     }
 }
